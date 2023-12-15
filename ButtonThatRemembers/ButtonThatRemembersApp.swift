6439b049afceda48be80a5d9abcb91b4920812d4
Oleg Frolov <frolololeg@gmail.com>
2023-12-15 23:20:30 +0000

    Initial Commit

---
+++ ButtonThatRemembers/ButtonThatRemembersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonThatRemembersApp.swift
+//  ButtonThatRemembers
+//
+//  Created by Oleg Frolov on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonThatRemembersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonThatRemembers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ButtonThatRemembers
+//
+//  Created by Oleg Frolov on 15/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
