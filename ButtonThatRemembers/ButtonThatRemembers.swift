94c81b8ba295e888757dc0aa751c5064df8dbfec
Oleg Frolov <frolololeg@gmail.com>
2023-12-16 17:49:14 +0000

    finger printing works

--- ButtonThatRemembers/ButtonThatRemembers.swift
+++ ButtonThatRemembers/ButtonThatRemembers.swift
@@ -7,20 +7,66 @@
 
 import SwiftUI
 
-struct ButtonThatRemembers: View 
+struct FingerTipPrint: View
+{
+    var position: Coords
+    @State private var angleOffset: Double = 0
+    @State private var opacityOffset: Double = 0
+    @State private var imageName: String = "Finger Print 5"
+    
+    var body: some View
+    {
+        ZStack()
+        {
+            Image(imageName)
+                .resizable()
+                .rotationEffect(.degrees(angleOffset))
+                .opacity(opacityOffset)
+                .scaleEffect(0.75)
+                .offset(y: 16)
+   
+        }
+        .blendMode(.lighten)
+        .frame(width: 128, height: 128)
+        .cornerRadius(8)
+//        .background(.red)
+        .position(x: position.x, y: position.y)
+        .onAppear()
+        {
+            angleOffset = Double.random(in: -20...20)
+            opacityOffset = Double.random(in: 0.4...0.6)
+            imageName = "Finger Print \(Int.random(in: 1..<5))"
+        }
+        
+
+    }
+}
+
+struct Coords: Hashable
+{
+    var x: Double
+    var y: Double
+}
+
+struct ButtonThatRemembers: View
 {
     
     @State private var isPressed: Bool = false
+    @State private var fingerPrints: [Coords] = []
     
     var body: some View 
     {
         VStack
         {
-            VStack
+            ZStack
             {
                 Text("Button")
                     .font(.custom("Helvetica", size: 32))
                     .foregroundStyle(.white)
+                
+                ForEach(fingerPrints, id: \.self) { fingerPrint in
+                    FingerTipPrint(position: fingerPrint)
+                }
             }
             .frame(width: 292, height: 96)
             
@@ -32,16 +78,19 @@ struct ButtonThatRemembers: View
             .onTapGesture
             {
                 location in
-                print("Tapped at \(location)")
+//                print("Tapped at \(location)")
+//                fingerPrints.append(Coords(x: location.x, y: location.y))
             }
         }
         .simultaneousGesture(DragGesture(minimumDistance: 0)
         .onChanged
-        { _ in
+        { touchData in
             if (!isPressed)
             {
                 print("Down")
                 isPressed = true
+                print(touchData.location)
+                fingerPrints.append(Coords(x: touchData.location.x, y: touchData.location.y))
             }
         }
         .onEnded
@@ -56,5 +105,10 @@ struct ButtonThatRemembers: View
 }
 
 #Preview {
-    ButtonThatRemembers()
+    VStack()
+    {
+        ButtonThatRemembers()
+        FingerTipPrint(position: Coords(x: 100, y: 100))
+    }
+    
 }
